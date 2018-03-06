4dff30a82740379e00df90f04132197798dd254a
olegtrofimenko <oleg.trofimenko@lykke.com>
2018-03-06 17:59:55 +0500

    rename

---
+++ src/Lykke.Service.PayBackoffice/wwwroot/js/mainmenu.js
@@ -0,0 +1,22 @@
+var MainManu = (function () {
+    function MainManu() {
+    }
+    MainManu.prototype.selectMenu = function (newElement) {
+        this.deselectMenu();
+        this.currentItem = newElement;
+        newElement.addClass('smiSel');
+    };
+    MainManu.prototype.selectByUrl = function (url) {
+        var element = $('li[data-url="' + url + '"]');
+        this.selectMenu(element);
+    };
+    MainManu.prototype.deselectMenu = function () {
+        if (this.currentItem) {
+            this.currentItem.removeClass('smiSel');
+            this.currentItem = undefined;
+        }
+    };
+    return MainManu;
+}());
+var mainMenu = new MainManu();
+//# sourceMappingURL=mainmenu.js.map
