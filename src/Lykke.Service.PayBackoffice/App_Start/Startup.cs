c828e541698784a6e46674d591f3556c08a427b3
olegtrofimenko <oleg.trofimenko@lykke.com>
2018-03-13 11:01:27 +0500

    fix settings

--- src/Lykke.Service.PayBackoffice/App_Start/Startup.cs
+++ src/Lykke.Service.PayBackoffice/App_Start/Startup.cs
@@ -23,8 +23,9 @@ namespace BackOffice
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                .AddEnvironmentVariables();
 
             builder.AddEnvironmentVariables();
 
