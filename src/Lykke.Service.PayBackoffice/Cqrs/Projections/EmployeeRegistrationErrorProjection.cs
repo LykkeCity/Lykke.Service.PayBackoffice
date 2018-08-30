83665cfc99a8d0e4ad9001fe914f3758c58a1ea0
tarurar <tarutin_andrew@mail.ru>
2018-08-30 19:18:53 +0300

    added projection

---
+++ src/Lykke.Service.PayBackoffice/Cqrs/Projections/EmployeeRegistrationErrorProjection.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Common.Log;
+using JetBrains.Annotations;
+using Lykke.Common.Log;
+using Lykke.Service.PayInvoice.Contract.Events;
+
+namespace BackOffice.Cqrs.Projections
+{
+    public class EmployeeRegistrationErrorProjection
+    {
+        private readonly ILog _log;
+
+        public EmployeeRegistrationErrorProjection([NotNull] ILogFactory logFactory)
+        {
+            _log = logFactory.CreateLog(this);
+        }
+
+        public Task Handle(EmployeeRegistrationFailedEvent evt)
+        {
+            _log.Critical(
+                "Employee registration",
+                message: $"Failed to register new employee: {evt.Error}",
+                context: evt.Email);
+
+            return Task.CompletedTask;
+        }
+    }
+}
