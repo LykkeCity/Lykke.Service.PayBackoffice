4f5fb4d7d19e2ecb4a1ae867d879a5bd36772481
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-09-16 01:13:48 +0300

    Add settlement logs LPDEV-850

---
+++ src/Lykke.Service.PayBackoffice/Areas/LykkePay/Models/Settlement/SettlementSearchFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.Service.PayMerchant.Client.Models;
+using System.Collections.Generic;
+
+namespace BackOffice.Areas.LykkePay.Models.Settlement
+{
+    public class SettlementSearchFormViewModel : ContinuationFormViewModel
+    {      
+        public string MerchantId { get; set; }
+
+        public IReadOnlyList<MerchantModel> Merchants { get; set; }
+
+        public string Query { get; set; }
+    }
+}
