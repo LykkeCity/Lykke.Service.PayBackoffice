764707dc425dcc633b31b33ff1a82395a645ca87
tarurar <tarutin_andrew@mail.ru>
2018-10-03 13:45:29 +0300

    added ui to manage BIL transfer settings

---
+++ src/Lykke.Service.PayBackoffice/Areas/LykkePay/Models/Merchants/MerchantTransferSettingsListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using BackOffice.Models;
+using Lykke.Service.PayMerchant.Client.Models;
+using Lykke.Service.PayTransferValidation.Client.Models.MerchantConfiguration;
+
+namespace BackOffice.Areas.LykkePay.Models.Merchants
+{
+    public class MerchantTransferSettingsListViewModel : PagedListModel
+    {
+        public IEnumerable<MerchantModel> Merchants { get; set; }
+        public bool IsEditAccess { get; set; }
+        public bool IsFullAccess { get; set; }
+        public string SelectedMerchant { get; set; }
+        public IEnumerable<LineModel> Settings { get; set; }
+    }
+}
