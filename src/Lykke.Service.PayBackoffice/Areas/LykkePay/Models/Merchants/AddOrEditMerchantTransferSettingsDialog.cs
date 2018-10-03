9c7fc39a68bf0f6126d900a68a8664ed874c8dd4
tarurar <tarutin_andrew@mail.ru>
2018-10-03 13:45:29 +0300

    added ui to manage BIL transfer settings

---
+++ src/Lykke.Service.PayBackoffice/Areas/LykkePay/Models/Merchants/AddOrEditMerchantTransferSettingsDialog.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using BackOffice.Models;
+using Lykke.Service.PayTransferValidation.Client.Models.Rule;
+
+namespace BackOffice.Areas.LykkePay.Models.Merchants
+{
+    public class AddOrEditMerchantTransferSettingsDialog : IPersonalAreaDialog
+    {
+        public string Caption { get; set; }
+        public string Width { get; set; }
+        public string MerchantId { get; set; }
+        public string RuleId { get; set; }
+        public string RuleInput { get; set; }
+        public bool Enabled { get; set; }
+        public bool IsNew { get; set; }
+        public IEnumerable<RegisteredRuleModel> RuleList { get; set; }
+    }
+}
